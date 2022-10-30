0f4966f4bd51155634284d42b28d7b6483dc2a71
EC2 Default User <ec2-user@ip-172-31-9-85.ap-northeast-1.compute.internal>
2022-10-30 13:05:35 +0000

    1st commit

---
+++ .c9/metadata/preview-https:/603067897f164cfc9a012e26214cf56a.vfs.cloud9.ap-northeast-1.amazonaws.com
@@ -0,0 +1,1 @@
+{"changed":false,"filter":false,"title":"[P] https://603067897f164cfc9a012e26214cf56a.vfs.cloud9.ap-northeast-1.amazonaws.com","value":"","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://603067897f164cfc9a012e26214cf56a.vfs.cloud9.ap-northeast-1.amazonaws.com","previewer":"previewBrowser","disableInjection":true}}
---
+++ 2章/app.js
@@ -0,0 +1,61 @@
+let hello = 'Hello World';
+alert(hello);
+
+// 整数を代入する
+// let int1 = 1;
+
+// 負数を代入する
+// let int2 = -10;
+
+// 小数点を代入する
+// let float1 = 3.14;
+
+// 文字列を代入する
+// let str1 = 'JavaScriptを覚えよう';
+
+// 足し算
+// alert(4 + 3);
+
+// 引き算
+// alert(8 - 5);
+
+// 掛け算
+// alert(2 * 6);
+
+// 割り算
+// alert(10 / 2);
+
+// let str1 = 'Hello';
+// let str2 = 'World!!';
+// alert(str1 + str2);
+
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// let i;
+// let num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
